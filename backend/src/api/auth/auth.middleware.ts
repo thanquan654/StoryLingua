ba70b9b7702ee0f6167fc37f1323befc6912c22b
Quan Than Van <quantv654@gmail.com>
2025-12-13 23:03:59 +0700

    feat: implement middleware to authenticate user

--- backend/src/api/auth/auth.middleware.ts
+++ backend/src/api/auth/auth.middleware.ts
@@ -1,5 +1,7 @@
 import type { Request, Response, NextFunction } from 'express'
 import { validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
+import envVar from '../../config/envVar.js'
 
 export const validate = (req: Request, res: Response, next: NextFunction) => {
 	const errors = validationResult(req)
@@ -8,4 +10,50 @@ export const validate = (req: Request, res: Response, next: NextFunction) => {
 	}
 
 	return res.status(400).json({ errors: errors.array() })
+}
+
+interface UserPayload {
+	userId: string
+	email: string
+}
+
+declare global {
+	namespace Express {
+		interface Request {
+			user?: UserPayload
+		}
+	}
+}
+
+export const authenticateUser = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const authHeader = req.headers['authorization']
+
+		if (!authHeader || !authHeader.startsWith('Bearer ')) {
+			return res.status(401).json({
+				message: 'Access denied. No token provided or invalid format.',
+			})
+		}
+
+		const token = authHeader.split(' ')[1]
+
+		if (!token) {
+			return res.status(401).json({
+				message: 'Access denied. No token provided or invalid format.',
+			})
+		}
+
+		// Verify token
+		const decoder = jwt.verify(token, envVar.jwtSecret) as UserPayload
+
+		req.user = decoder
+
+		next()
+	} catch (error) {
+		return res.status(403).json({ message: 'Invalid or expired token.' })
+	}
 }
