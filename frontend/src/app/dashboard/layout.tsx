2aff1e50f2b017980ba7ee4039b56312758dcaff
Quan Than Van <quantv654@gmail.com>
2025-12-13 16:43:25 +0700

    feat: add sidebar for pc

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,11 +1,15 @@
 import BottomNavigation from '@/components/BottomNavigation'
+import Sidebar from '@/components/Sidebar'
 import React, { ReactNode } from 'react'
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
 	return (
-		<div>
-			<div>{children}</div>
+		<div className="flex min-h-screen bg-[#0F172A]">
+			<div className="flex-1 w-full md:pl-56 transition-[padding] duration-300 relative isolate">
+				{children}
+			</div>
 			<BottomNavigation />
+			<Sidebar />
 		</div>
 	)
 }
