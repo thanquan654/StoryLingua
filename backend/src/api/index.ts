ba3bb61ca566a018bc63ceacfcb37c53f6b3cef5
Quan Than Van <quantv654@gmail.com>
2025-12-18 16:18:50 +0700

    feat: return more data in getProfile API

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 import authRouter from './auth/auth.route.js'
-import userRouter from './user/user.route.js'
+import userRouter from './users/user.route.js'
 
 const router = Router()
 
 router.use('/auth', authRouter)
-router.use('/user', userRouter)
+router.use('/users', userRouter)
 
 router.use('/healthcheck', (req, res) => {
 	res.status(200)
